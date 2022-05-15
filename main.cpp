f069aecd173896ef889ef7abcccc0f773422f0f5
SarthakJain01 <88705836+SarthakJain01@users.noreply.github.com>
2022-05-15 22:08:38 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,68 +6,40 @@
 #include <vector>
 using namespace std;
 
-int main()
+void print(vector<int>list)
 {
-
-vector<int>Array1({1,3,5,4,-5,100,7777,2014});
-vector<int>Array2({0,3,5,4,-5,100,7777,2014});
-BubbleSort Method;
-QuickSort Method2;
-RecursiveBinarySearch Method3;
-vector<int>result1 = Array1;
-bool result2 = 0;
-bool result4 = 0;
-vector<int>result3 = Array2;
-
-result2 = Method3.search(Array1,1);
-if(result2 == 1)
-{
-cout<<"true ";
-}
-else
-{
-cout<<"false ";
+    for(int i = 0;i<list.size();i++)
+    {
+        cout<<list[i]<<" ";
+    }
+    cout<<endl;
 }
 
-result1 = Method.sort(result1);
-for(int x = 0;x<result1.size();x++){
-    cout<<result1.at(x)<<" ";
-};
-cout<<""<<endl;
+vector<int>input(){
+    vector<int>list;
+    int number;
+    while(cin>>number){
+        list.push_back(number);
+    }
+    return list;
+}
 
-result4 = Method3.search(Array2,1);
-if(result4 == 1)
+int main(void)
 {
-cout<<"true ";
-}
-else
-{
-cout<<"false ";
-}
-result3 = Method2.sort(result3);
-for(int x = 0;x<result3.size();x++){
-    cout<<result3.at(x)<<" ";
-};
-cout<<""<<endl;
+    vector<int>list;
 
+    list = input();
+    QuickSort Method;
+    list = Method.sort(list);
+    RecursiveBinarySearch Method2;
+    bool result = Method2.search(list,1);
+    if(result == 1)
+    {
+        cout<<"true ";
+    }
+    else
+    {
+        cout<<"false ";
+    }
+    print(list);
 }
-// vector<int>Array2({1,3,5,4,-5,100,7777,2014});
-// QuickSort Method2;
-// Array2 = Method2.sort(Array2);
-// for(int x = 0;x<Array2.size();x++){
-//     cout<<Array2.at(x)<<" ";
-// };
-// cout<<""<<endl;
-
-// vector<int>Array3({1,3,5,4,-5,100,7777,2014});
-// RecursiveBinarySearch Method3;
-// bool result = Method3.search(Array3,2);
-// if(result == 1)
-// {
-// cout<<"true"<<endl;
-// }
-// else
-// {
-// cout<<"false"<<endl;
-// }
-// }
